18b59704acab2dd9bccd7f684ea8beb7ade3c2c6
Filipe Ramos <carlosfilipesoaresramos2001@gmail.com>
2026-05-04 16:41:54 -0300

    refactor(aside): add sidebar config and conditioal rendering

---
+++ src/config/sidebar.ts
@@ -0,0 +1,29 @@
+export const sidebar = [
+  {
+    title: 'Getting Started',
+    href: '/',
+    items: [
+      { title: 'What is Fluffle?', href: '/' },
+      { title: 'Installation setup', href: '/introduction/installation' },
+    ],
+  },
+  {
+    title: 'Features',
+    href: '/features/stylesheet-api',
+    items: [
+      { title: 'StyleSheet API', href: '/features/stylesheet-api' },
+      { title: 'Nesting', href: '/features/nesting' },
+      { title: 'Shorthand properties', href: '/features/shorthands' },
+      { title: 'Mixins', href: '/features/mixins' },
+    ],
+  },
+  {
+    title: 'Contributing',
+    href: '/contributing/issues',
+    items: [
+      { title: 'Issues and bugs', href: '/contributing/issues' },
+      { title: 'Pull requests', href: '/contributing/pull-requests' },
+      { title: 'Donate', href: '/contributing/donate' },
+    ],
+  },
+];
